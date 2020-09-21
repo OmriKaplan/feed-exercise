03341512029c37a5028f147f74d127b842877803
Omri Kaplan <okaplan@lightricks.com>
2020-09-21 18:57:53 +0300

    Data: implement the data access layer.

--- app/src/main/java/com/lightricks/feedexercise/database/DataAccessObjects.kt
+++ app/src/main/java/com/lightricks/feedexercise/database/DataAccessObjects.kt
@@ -1,5 +1,20 @@
 package com.lightricks.feedexercise.database
 
-/***
- * todo: add Room's Data Access Object interface(s) here
- */
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import io.reactivex.Completable
+
+@Dao
+interface FeedItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertFeedItems(feedItemEntities: List<FeedItemEntity>): Completable
+
+    @Query("DELETE FROM feedItems")
+    fun clearTable(): Completable
+
+    @Query("SELECT * FROM feedItems")
+    fun getAll(): LiveData<List<FeedItemEntity>>
+
+    @Query("SELECT COUNT(*) FROM feedItems")
+    fun feedItemsCount(): Int
+}
