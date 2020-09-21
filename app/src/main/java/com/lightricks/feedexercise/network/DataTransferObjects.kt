7a541f937102502c8bbb3be887b378602ad4280b
Omri Kaplan <okaplan@lightricks.com>
2020-09-21 18:54:08 +0300

    FeedApiService: implement the service.

--- app/src/main/java/com/lightricks/feedexercise/network/DataTransferObjects.kt
+++ app/src/main/java/com/lightricks/feedexercise/network/DataTransferObjects.kt
@@ -1,5 +1,17 @@
 package com.lightricks.feedexercise.network
 
-/**
- * todo: add Data Transfer Object data class(es) here
- */
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class TemplatesMetadataItem(
+    val configuration: String,
+    val id: String,
+    val isNew: Boolean,
+    val isPremium: Boolean,
+    val templateCategories: List<String>,
+    val templateName: String,
+    val templateThumbnailURI: String
+)
+
+@JsonClass(generateAdapter = true)
+data class GetFeedResponse(val templatesMetadata: List<TemplatesMetadataItem>)
